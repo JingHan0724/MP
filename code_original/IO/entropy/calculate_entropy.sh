158e51a1594b331a6f146bcf5ca640df2de3ad33
Unusz <91433298+Unusz@users.noreply.github.com>
2023-09-18 13:03:08 +0200

    Add files via upload

--- code_original/IO/entropy/calculate_entropy.sh
+++ code_original/IO/entropy/calculate_entropy.sh
@@ -3,15 +3,16 @@
 monitor_interval=10 # Set the monitor interval in seconds
 
 cleanup(){
-    rm -f lsof_info.txt
-    exit 0
+  rm -f lsof_info.txt
+  rm -rf ${ScriptDir}/tmp
+  exit 0
 }
 
 trap cleanup SIGINT
 
 #Server and port to push data
-server="http://192.168.1.100"
-port="5004"
+server="http://192.168.31.185"
+port="5002"
 directory="/sensor/"
 mac=$( cat /sys/class/net/eth0/address | tr : _ )
 
@@ -34,18 +35,22 @@ EOF
 
 monitor_write_file(){
   while true; do
-    title="========== Monitoring Round: $(date '+%F +%T') =========="
-    res=$(curl -sk -X POST -d "$title" -H "Content-Type: application/json" "$server:$port$directory$mac")	
+    title="========== Monitoring Round timestamp: $(($(date +%s%N)/1000000)) =========="
+    res=$(curl -sk -X POST -d "$title" -H "Content-Type: application/json" "$server:$port$directory$mac")	    
     lsof >${lsof_file}
-    cat ${lsof_file}| tr -s ' ' ','|grep ',REG,'|grep ',[0-9]\+[uw],'|awk -F',' '{print $NF}'|sort -u|while read -r Line;do
+    mkdir -p ${ScriptDir}/tmp
+    file_path="${ScriptDir}/tmp/.tmp_entropy.file" 
+    cat ${lsof_file}| tr -s ' ' ','|grep ',REG,'|grep ',[0-9]\+[uw],'|grep -v '(deleted)'|awk -F',' '{print $NF}'|sort -u|while read -r Line;do
       #echo "========Caculate Entropy File[ ${Line} ]"
-      file_path="${Line}"
+      head -c ${header_byte} "${Line}" >${ScriptDir}/tmp/.tmp_entropy.file       
       caculate_entropy
       #PUSH to server	
-      finalOutput="entropy: $(echo ${entropy}|awk  '{printf "%.6f\n", $0}') | filePath: [ ${file_path} ]"
+      finalOutput="entropy: $(echo ${entropy}|awk  '{printf "%.6f\n", $0}') | filePath: [ ${Line} ]"
+      #echo "${finalOutput}"
       res=$(curl -sk -X POST -d "$finalOutput" -H "Content-Type: application/json" "$server:$port$directory$mac")
     done
-    rm -rf ${lsof_file}    
+    rm -rf ${lsof_file}
+    rm -rf ${ScriptDir}/tmp    
     sleep "${monitor_interval}"
   done
 }
@@ -53,4 +58,6 @@ monitor_write_file(){
 ScriptDir=$(cd `dirname $0`; pwd)
 pythoncmd="python3"
 lsof_file="${ScriptDir}/lsof_info.txt"
+header_byte=100 #the amount of bytes to calculate; or select randomly: header_byte=`echo $((RANDOM % 101 + 100))` 
 monitor_write_file
+

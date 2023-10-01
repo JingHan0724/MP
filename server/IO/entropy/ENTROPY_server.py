46954b9f5400aaa5125d03629b84b6f518bd2d80
Unusz <91433298+Unusz@users.noreply.github.com>
2023-10-01 23:47:53 +0200

    Add files via upload

--- server/IO/entropy/ENTROPY_server.py
+++ server/IO/entropy/ENTROPY_server.py
@@ -14,32 +14,38 @@ elif platform.system() == "Windows":
 
 app = Flask(__name__)
 api = Api(app)
-data_directory = str(os.getcwd()) + bar + "entropy_data"
+data_directory = str(os.getcwd()) + bar + "Entropy"
+
+fieldnames = ["timestamp", "file_path", "action", "entropy"]
 
 class sensor(Resource):
 
     def post(self, sensorid):
         vector = request.data.decode("utf-8")
+        
+        # Vector should be a comma-separated string with data
+        data = vector.split(',')
 
-        vector = vector + "\n"
-        file_path = data_directory + bar + "{}.txt".format(sensorid)
+        file_path = data_directory + bar + "{}.csv".format(sensorid)
         
         # Check if the file exists, and if not, create it with headers
         if not os.path.exists(file_path):
-            os.system(r"touch {}".format(sensorid))
+            with open(file_path, "w") as csvfile:
+                csv_write = csv.writer(csvfile)
+                csv_write.writerow(fieldnames)
 
-        # Append the data to the file
-        file = open(file_path, "a")
-        file.write(vector)
-        file.close()
-
+        # Append the data to the CSV file
+        with open(file_path, "a") as csvfile:
+            csv_write = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            data_dict = dict(zip(fieldnames, data))
+            csv_write.writerow(data_dict)
 
 def launch_REST_Server():
     if not os.path.exists(data_directory):
         os.makedirs(data_directory)
 
     api.add_resource(sensor, '/sensor/<sensorid>')  # Route_1
-    http_server = WSGIServer(('0.0.0.0', 5002), app)
+    http_server = WSGIServer(('0.0.0.0', 5003), app)
     http_server.serve_forever()
 
 if __name__ == "__main__":
--- server/IO/entropy/calculate_entropy.sh
+++ server/IO/entropy/calculate_entropy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-monitor_interval=10 # Set the monitor interval in seconds
+# specify which directory to monitor
+monitor_dir="/"
 
 cleanup(){
-  rm -f lsof_info.txt
   rm -rf ${ScriptDir}/tmp
   exit 0
 }
@@ -12,7 +12,7 @@ trap cleanup SIGINT
 
 #Server and port to push data
 server="http://192.168.31.185"
-port="5002"
+port="5003"
 directory="/sensor/"
 mac=$( cat /sys/class/net/eth0/address | tr : _ )
 
@@ -20,44 +20,45 @@ caculate_entropy(){
 entropy=$(${pythoncmd} <<EOF
 import math
 from collections import Counter
-with open('$file_path', 'rb') as f:
+try:
+  with open('$tmp_path', 'rb') as f:
     data = f.read()
-counter = Counter(data)
-total_bytes = len(data)
-entropy = 0
-for count in counter.values():
-  p_x = count / total_bytes
-  entropy += - p_x * math.log2(p_x)
-print(entropy)
+    counter = Counter(data)
+    total_bytes = len(data)
+    entropy = 0
+    for count in counter.values():
+      p_x = count / total_bytes
+      entropy += - p_x * math.log2(p_x)
+    print(entropy)
+except FileNotFoundError:
+  pass
 EOF
 )
 }
 
 monitor_write_file(){
-  while true; do
-    title="========== Monitoring Round Timestamp: $(($(date +%s%N)/1000000)) =========="
-    res=$(curl -sk -X POST -d "$title" -H "Content-Type: application/json" "$server:$port$directory$mac")	    
-    lsof >${lsof_file}
-    mkdir -p ${ScriptDir}/tmp
-    file_path="${ScriptDir}/tmp/.tmp_entropy.file" 
-    cat ${lsof_file}| tr -s ' ' ','|grep ',REG,'|grep ',[0-9]\+[uw],'|grep -v '(deleted)'|awk -F',' '{print $NF}'|sort -u|while read -r Line;do
-      #echo "========Caculate Entropy File[ ${Line} ]"
-      head -c ${header_byte} "${Line}" >${ScriptDir}/tmp/.tmp_entropy.file       
+  inotifywait -q -m -e modify,create --fromfile ${ScriptDir}/fromfile.txt -r $monitor_dir | while read path action file
+  do
+    if [[ -f "$path$file" && "$file" != *"log"* && "$file" != *"watchdog"* && "$file" != *".swp"* && "$file" != *".tmp"* && "$file" != *".swx"* ]]; then
+      timestamp=$(($(date +%s%N)/1000000))
+      file_path="$path$file"
+      head -c ${header_byte} "${file_path}" >${tmp_path}       
       caculate_entropy
-      #PUSH to server	
-      finalOutput="entropy: $(echo ${entropy}|awk  '{printf "%.6f\n", $0}') | filePath: [ ${Line} ]"
-      #echo "${finalOutput}"
-      res=$(curl -sk -X POST -d "$finalOutput" -H "Content-Type: application/json" "$server:$port$directory$mac")
-    done
-    rm -rf ${lsof_file}
-    rm -rf ${ScriptDir}/tmp    
-    sleep "${monitor_interval}"
+		
+      finalOutput="[$timestamp] $file_path | $action | entropy: $(echo ${entropy}|awk  '{printf "%.6f\n", $0}')"
+      num="$(echo ${entropy} | awk '{printf "%.6f", $0}')"
+      #echo $num
+      final="$timestamp,$file_path,$action,$num"
+      echo $finalOutput
+      res=$(curl -sk -X POST -d "$final" -H "Content-Type: application/json" "$server:$port$directory$mac")
+    fi
   done
 }
 
 ScriptDir=$(cd `dirname $0`; pwd)
+mkdir -p ${ScriptDir}/tmp
+tmp_path=${ScriptDir}/tmp/entropy.txt
 pythoncmd="python3"
-lsof_file="${ScriptDir}/lsof_info.txt"
 header_byte=100 #the amount of bytes to calculate; or select randomly: header_byte=`echo $((RANDOM % 101 + 100))` 
 monitor_write_file
 

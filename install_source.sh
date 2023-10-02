cda2d509533b23546efd5d8721d10a4d33ec088b
Unusz <91433298+Unusz@users.noreply.github.com>
2023-10-02 03:17:47 +0200

    Update install_source.sh

--- install_source.sh
+++ install_source.sh
@@ -89,6 +89,8 @@ echo "Depencencies installed"
 echo "Installing dependencies for the BLOCK and ENTROPY Monitor"
 cd ..
 cd IO
+sudo apt-get install inotify-tools
+sudo sysctl fs.inotify.max_user_watches=65536
 chmod +x block_monitor.sh
 chmod +x calculate_entropy.sh
 echo "Depencencies installed"
